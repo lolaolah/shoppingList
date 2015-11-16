b478f834269d6bea6c2968b1af0a9bd1cc08cb17
Lola Olah <lolaolah@gmail.com>
2015-11-15 22:01:56 -0500

    final files

--- js/app.js
+++ js/app.js
@@ -1,48 +1,12 @@
-var del_btn = "<button class='fa fa-trash-o'></button>";
-var check_btn = "<button class='fa fa-check-circle-o'></button>";
+
+var check_btn = "<button class='fa fa-check'></button>";
 var times_btn = "<button class='fa fa-times'></button>";
 
+function addToList () {
+  var text = $("#addNewItem").val();
+  alert(text);
+}
 
-$(document).ready( function () {
-
-  $('#add_shopping').click( function(e) {
-    e.preventDefault();
-    var shoppingDescription = $('#shopping_description').val();
-
-    $('.shopping_list').append('<div class="shopping" >'
-
-        + '<div class="shopping_description">'
-            + shoppingDescription
-        + '</div>'
-
-        + '<div>'
-            + '<input class="check_shopping" name="check_shopping" type="checkbox" />'
-        + '</div>'
-
-        + '<div>'
-            + '<input class="delete_shopping" name="delete_shopping" type="checkbox" />'
-        + '</div>'
-
-    + '</div>');
-
-    $('#shopping_form')[0].reset();
-  });
+$(function() {
+  $("#addText").on('click', addToList);
 });
-
-/*$( "#item" ).submit(function( event ) {
-  event.preventDefault();
-  var x = $("input").val();
-  $('.BuyList').prepend("<li>"+x+del_btn+check_btn+"</li>");
-});
-
-
-$(document).ready( function () {
-
-  // When the button is clicked
-  $("#button").click(function(){
-
-    // Prepend this HTML
-    $("#BuyList").prepend("<li>Apples</li>");
-  })
-});
-*/
