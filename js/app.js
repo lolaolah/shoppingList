f5fbb0fcce7d45b5079564ec80b436b8020d15bf
Lola Olah <lolaolah@gmail.com>
2015-11-18 08:33:24 -0500

    added the enter key

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 
-var check_btn = "<button class='fa fa-check'></button>";
-var times_btn = "<button class='fa fa-times'></button>";
-
 function addToList () {
   var text = $("#addNewItem").val();
   alert(text);
